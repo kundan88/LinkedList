0d69f0a1423a06a024e015c7be8d3aee6334bb5e
Kundan <kundan kamble>
2022-08-13 09:02:56 -0800

    Simple LinkedList Program

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            //Check wether list is empty or not then create node as head
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {              
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+                Console.WriteLine("Inserted Data: " + node.data);
+            }
+
+        }
+        public void Display()
+        {
+            //temp variable will head
+            Node temp = head;
+            //Check wether list is empty or not
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList Is Empty");
+            }
+            else
+            {
+                //Check until temp will null
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.data + " ");
+                    //for traversing the node
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
+
---
+++ LinkedList/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LinkedList
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();            
+            list.Add(56);
+            list.Add(30);
+            list.Add(76);
+            list.Display();
+        }
+    }
+}
